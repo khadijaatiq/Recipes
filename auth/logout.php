e2457d5da90ff10be31daf7eb5076f8a83984cbf
ayesharazaa <166994895+ayesharazaa@users.noreply.github.com>
2025-05-01 21:28:07 +0500

    Update logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -4,11 +4,13 @@ session_destroy();
 ?>
 
 <?php include '../partials/header.php'; ?>
-
+<main class="main-content">
 <div class="logout-container">
     <h2>You have been logged out!</h2>
     <p>Thank you for visiting. See you next time.</p>
     <a href="../auth/login.php" class="btn">Login Again</a>
 </div>
+</main>
 
 <?php include '../partials/footer.php'; ?>
+
